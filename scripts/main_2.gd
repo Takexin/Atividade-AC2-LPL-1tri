d0828dcb40ae0545404a91f3fe5121601348e9de
Felipe Takeshi <167320163+Takexin@users.noreply.github.com>
2025-04-01 17:31:09 -0300

    cutscene change + background sprites

--- scripts/main_2.gd
+++ scripts/main_2.gd
@@ -4,3 +4,6 @@ extends Node2D
 func _ready() -> void:
 	$AnimationPlayer.play( "start_anim")
 	#DialogueManager.show_dialogue_balloon(dialoguePath, "start")
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("reset"):
+		get_tree().reload_current_scene()
